504145dc123ef221619259cfa0f880d7f47de065
Nabeel Ali <mail2nabeelali@gmail.com>
2024-07-22 12:18:20 +0500

    packages update

--- index.php
+++ index.php
@@ -5,83 +5,86 @@ $start = microtime(true);
 use IconicCodes\LightView\LightView;
 
 include "app/config/general.php";
+
 include "class/TinyHTML.php";
 include "class/LightView.php";
 
 $configs = [];
-$mode = 'dev';
+$mode = "dev";
 
 $view = new LightView();
 $view->views_path = __DIR__ . "/app/";
 $view->includes_path = __DIR__ . "/app/includes/";
-$view->cache_enabled = ($mode == 'prod');
+$view->cache_enabled = $mode == "prod";
 
-function redirect($url) {
+function redirect($url)
+{
     if (!headers_sent()) {
-        header('Location: ' . $url);
+        header("Location: " . $url);
         exit();
     } else {
         echo '<script type="text/javascript">';
         echo 'window.location.href="' . $url . '";';
-        echo '</script>';
-        echo '<noscript>';
+        echo "</script>";
+        echo "<noscript>";
         echo '<meta http-equiv="refresh" content="0;url=' . $url . '" />';
-        echo '</noscript>';
+        echo "</noscript>";
         exit();
     }
 }
 
-
-function refresh($url) {
+function refresh($url)
+{
     echo "<meta http-equiv='refresh' content='0; url=$url'>";
 }
 
-function view($file, $data) {
+function view($file, $data)
+{
     global $view;
     return $view->view($file, $data);
 }
 
-function json($data) {
-    header('Content-Type: application/json');
+function json($data)
+{
+    header("Content-Type: application/json");
     return json_encode($data);
 }
 
-function clearRouteName($route = '') {
-    $route = trim(preg_replace('~/{2,}~', '/', $route), '/');
-    return $route === '' ? '/' : "/{$route}";
+function clearRouteName($route = "")
+{
+    $route = trim(preg_replace("~/{2,}~", "/", $route), "/");
+    return $route === "" ? "/" : "/{$route}";
 }
 
-function makeRequestUri() {
+function makeRequestUri()
+{
     // $requestURI = strtolower($_SERVER['REQUEST_URI']);
     // if (strpos($requestURI, '.') !== false) {
     //     return false;
     // }
-    $requestURI = $_SERVER['REQUEST_URI'];
-    $script = $_SERVER['SCRIPT_NAME'];
+    $requestURI = $_SERVER["REQUEST_URI"];
+    $script = $_SERVER["SCRIPT_NAME"];
     $dirname = dirname($script);
-    $dirname = $dirname === '/' ? '' : $dirname;
+    $dirname = $dirname === "/" ? "" : $dirname;
     $basename = basename($script);
     $uri = str_replace([$dirname, $basename], "", $requestURI);
-    $uri = clearRouteName(explode('?', $uri)[0]);
-    define('URI', $uri);
+    $uri = clearRouteName(explode("?", $uri)[0]);
+    define("URI", $uri);
     return $uri;
 }
 
-
 $uri = makeRequestUri();
 
-
-if (strpos($uri, '__layout') !== false) {
-    $view->view('pages/404.html');
+if (strpos($uri, "__layout") !== false) {
+    $view->view("pages/404.html");
     return;
 }
 
-if ($uri == '/') {
-    $uri = '/index';
+if ($uri == "/") {
+    $uri = "/index";
 }
 
-
-$method = $_SERVER['REQUEST_METHOD'];
+$method = $_SERVER["REQUEST_METHOD"];
 $method = strtolower($method);
 
 $success = false;
@@ -95,19 +98,18 @@ $success = false;
 
 // spl_autoload_register('customAutoloader');
 
+$uriParts = explode("/", trim($uri, "/"));
+$class = array_shift($uriParts) ?? "index";
+$class = preg_replace("/[^a-zA-Z0-9]/", "", $class);
 
-$uriParts = explode('/', trim($uri, '/'));
-$class = array_shift($uriParts) ?? 'index';
-$class = preg_replace('/[^a-zA-Z0-9]/', '', $class);
-
-$classFile = 'app/pages/' . $class . '.php';
+$classFile = "app/pages/" . $class . ".php";
 if (file_exists($classFile)) {
     require_once $classFile;
     $fullyQualifiedClass = "Pages\\$class";
     if (class_exists($fullyQualifiedClass)) {
-        $classMethod = array_shift($uriParts) ?? 'index';
+        $classMethod = array_shift($uriParts) ?? "index";
         $args = $uriParts ?? [];
-        $classMethod = preg_replace('/[^a-zA-Z0-9]/', '', $classMethod);
+        $classMethod = preg_replace("/[^a-zA-Z0-9]/", "", $classMethod);
         $classMethod = $method . ucfirst($classMethod);
         if (method_exists($fullyQualifiedClass, $classMethod)) {
             $instance = new $fullyQualifiedClass();
@@ -123,11 +125,11 @@ if (file_exists($classFile)) {
             $success = false;
         }
     }
-} elseif (file_exists('app' . $uri . '.php')) {
-    $page = 'app' . $uri . '.php';
-    $namespace = "app" . str_replace('/', "\\", $uri);
+} elseif (file_exists("app" . $uri . ".php")) {
+    $page = "app" . $uri . ".php";
+    $namespace = "app" . str_replace("/", "\\", $uri);
     include $page;
-    $execMethod = $namespace . '\\' . $method;
+    $execMethod = $namespace . "\\" . $method;
     if (function_exists($execMethod)) {
         $output = $execMethod();
         $success = true;
@@ -138,22 +140,22 @@ if (file_exists($classFile)) {
             $success = false;
         }
     }
-} elseif (file_exists('app/pages' . $uri . '.html')) {
-    $page = ltrim($uri, '/') . '.html';
-    $view->view('pages/' . $page);
+} elseif (file_exists("app/pages" . $uri . ".html")) {
+    $page = ltrim($uri, "/") . ".html";
+    $view->view("pages/" . $page);
     $success = true;
-} elseif (file_exists('app/pages/' . $uri . '/index.html')) {
-    $page = ltrim($uri, '/') . '/index.html';
-    $view->view('pages/' . $page);
+} elseif (file_exists("app/pages/" . $uri . "/index.html")) {
+    $page = ltrim($uri, "/") . "/index.html";
+    $view->view("pages/" . $page);
     $success = true;
-} elseif (file_exists('app/pages' . $uri . '.md')) {
-    $page = ltrim($uri, '/') . '.md';
-    $view->view('pages/' . $page);
+} elseif (file_exists("app/pages" . $uri . ".md")) {
+    $page = ltrim($uri, "/") . ".md";
+    $view->view("pages/" . $page);
     $success = true;
 }
 
 if (!$success) {
-    $view->view('pages/404/index.html');
+    $view->view("pages/404/index.html");
 }
 // echo '<div style="border: 1px solid #333;position: fixed; left: 0; bottom: 0; z-index: 9999; padding: 10px; background-color: white">';
 // $memory_size = memory_get_usage();
